772668c8fbcd6048387a0709fce5a42d1003faa7
Toni Lucas <alucas@knoxr1.us>
2018-10-25 12:15:17 -0500

    First Commit
    Paddle, Ball, Blocks, and Barriers in place.  Next step is to fix some bugs with the world space by moving the camera

---
+++ Physics/Assets/_Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xVelocity = 5f;
+    [SerializeField] float yVelocity = 15f;
+
+    Vector2 paddleToBallDistance;
+    bool hasStarted;
+
+	// Use this for initialization
+	void Start () 
+    {
+        paddleToBallDistance = transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            BallStuckToPaddle();
+            LaunchBallOnClick();
+        }
+
+    }
+
+    private void LaunchBallOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity, yVelocity);
+        }
+    }
+
+    private void BallStuckToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallDistance;
+    }
+}
---
+++ Physics/Assets/_Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    [SerializeField] float minX = -7f;
+    [SerializeField] float maxX = 7f;
+    [SerializeField] float screenWidthUnits = 16f;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float mousePosUnits = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosUnits, minX, maxX);
+        transform.position = paddlePos;
+	}
+}
