87e159d84f785965da5ce7c903940e53578a1965
Toni Lucas <alucas@knoxr1.us>
2018-10-28 13:48:36 -0500

    Part 5 Commit
    These are all the changes I made during the Part 5 Video

---
+++ Physics/Assets/_Scripts/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+
+	}
+}
