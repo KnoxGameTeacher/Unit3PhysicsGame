8ad24a75972f21469313c1296945ac579e5657bd
Toni Lucas <alucas@knoxr1.us>
2018-10-30 15:37:18 -0500

    Part 9 Commit
    Added moving blocks functionality

---
+++ Physics/Assets/_Scripts/MovingRobot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRobot : MonoBehaviour {
+
+    [SerializeField] GameObject patrolBot;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] Transform currentDest;
+    [SerializeField] Transform[] destinations;
+    [SerializeField] int destSelect;
+
+	// Use this for initialization
+	void Start () 
+    {
+        currentDest = destinations[destSelect];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (patrolBot != null)
+        {
+            patrolBot.transform.position = Vector2.MoveTowards(patrolBot.transform.position, currentDest.position, moveSpeed * Time.deltaTime);
+
+            if (patrolBot.transform.position == currentDest.position)
+            {
+                destSelect++;
+
+                if (destSelect == destinations.Length)
+                {
+                    destSelect = 0;
+                }
+
+                currentDest = destinations[destSelect];
+            }
+        }
+	}
+}
