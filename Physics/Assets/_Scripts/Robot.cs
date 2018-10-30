fca245f65930e54e5a06520209198ee8c3f22c8a
Toni Lucas <alucas@knoxr1.us>
2018-10-30 08:52:24 -0500

    Part 8 Commit
    Gave the blocks an isBreakable variable, and added VFX and SFX to the moment the blocks break

---
+++ Physics/Assets/_Scripts/Robot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour 
+{
+
+    [SerializeField] bool isBreakable = false;
+    [SerializeField] GameObject electricityVFX;
+    [SerializeField] AudioClip explosionSFX;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(isBreakable == true)
+        {
+            DestroyBlock();
+        }
+    }
+
+    private void DestroyBlock()
+    {
+
+        GameObject electricity = Instantiate(electricityVFX, transform.position, transform.rotation);
+        AudioSource.PlayClipAtPoint(explosionSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+
+
+    }
+
+}
